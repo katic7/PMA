4f770c5c9e4a56dbb7001511a15f1f28686d017c
katic7 <katicmilan7@gmail.com>
2020-05-11 18:19:38 +0200

    dialog news event

--- app/src/main/java/ftn/tim34/weplay/EventInformationsActivity.java
+++ app/src/main/java/ftn/tim34/weplay/EventInformationsActivity.java
@@ -1,9 +1,13 @@
 package ftn.tim34.weplay;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -30,6 +34,24 @@ public class EventInformationsActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.event_news_list);
         listView.setAdapter(adapter);
+        Button mShowDialog = (Button) findViewById(R.id.btnShowDialog);
+        mShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(EventInformationsActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_event_message, null);
+                Button btnAddReview = (Button) mView.findViewById(R.id.btnAddReview);
+                btnAddReview.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View vi) {
+                        Toast.makeText(vi.getContext(), "News Successfuly added!", Toast.LENGTH_SHORT).show();
 
+                    }
+                });
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+            }
+        });
     }
 }
